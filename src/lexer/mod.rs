967c6e78a3bb24a0c84d59dae6d80c4b4169395b
Nearsyh <nearsy.h@gmail.com>
2019-01-06 15:00:30 +0800

    Handle simple tokens

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,12 +1,50 @@
 use super::token::{Token, TokenType};
 
 pub struct Lexer<'a> {
-  input: &'a str
+  input: &'a str,
+  chars: std::str::Chars<'a>,
+  position: u32,
+  read_position: u32,
+  ch: char
 }
 
 impl<'a> Lexer<'a> {
-  fn next_token(&self) -> Token {
-    return Token { token_type: TokenType::EOF, literal: ""};
+  fn new(input: &'a str) -> Lexer<'a> {
+    let mut ret = Lexer {
+      input: input, 
+      chars: input.chars(), 
+      position: 0, 
+      read_position: 0,
+      ch: '\0' };
+    ret.read_char();
+    return ret;
+  }
+
+  fn read_char(&mut self) {
+    if self.read_position >= (self.input.len() as u32) {
+      self.ch = '\0';
+    } else {
+      self.ch = self.chars.next().unwrap();
+    }
+    self.position = self.read_position;
+    self.read_position += 1;
+  }
+
+  fn next_token(&mut self) -> Token<'a> {
+    let tok = match self.ch {
+      '=' => Token::new(TokenType::ASSIGN, "="),
+      '+' => Token::new(TokenType::PLUS, "+"),
+      ',' => Token::new(TokenType::COMMA, ","),
+      ';' => Token::new(TokenType::SEMICOLON, ";"),
+      '(' => Token::new(TokenType::LPAREN, "("),
+      ')' => Token::new(TokenType::RPAREN, ")"),
+      '{' => Token::new(TokenType::LBRACE, "{"),
+      '}' => Token::new(TokenType::RBRACE, "}"),
+      '\0' => Token::new(TokenType::EOF, ""),
+      _ => Token::new(TokenType::ILLEGAL, "")
+    };
+    self.read_char();
+    return tok;
   }
 }
 
@@ -31,7 +69,7 @@ mod tests {
     ];
 
 
-    let l = Lexer { input: input };
+    let mut l = Lexer::new(input);
     for i in 0..tests.len() {
       let tok = l.next_token();
       let (expected_type, expected_literal) = &tests[i];
--- src/token/mod.rs
+++ src/token/mod.rs
@@ -26,3 +26,9 @@ pub struct Token<'a> {
   pub token_type: TokenType,
   pub literal: &'a str
 }
+
+impl<'a> Token<'a> {
+  pub fn new(token_type: TokenType, literal: &'a str) -> Token<'a> {
+    return Token { token_type: token_type, literal: literal };
+  }
+}
